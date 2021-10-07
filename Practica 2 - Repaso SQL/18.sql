21c54b8ac8de56d48b95e0b0780735a35449dbb4
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-10-07 17:57:39 +0200

    18 hecho x2

--- Practica 2 - Repaso SQL/18.sql
+++ Practica 2 - Repaso SQL/18.sql
@@ -1,3 +1,9 @@
 SELECT p.product_name, o.quantity FROM order_details AS o
 INNER JOIN (SELECT * FROM products WHERE units_on_order < 200) AS p
 ON p.product_id=o.product_id
+
+-- Segunda ainterpretación
+SELECT p.product_name, sum(o.quantity) FROM order_details AS o
+INNER JOIN (SELECT * FROM products WHERE units_on_order < 200) AS p
+ON p.product_id=o.product_id
+GROUP BY p.product_name
