77b569f8d601913b104bca947002f348c3740361
Yuki CS <34201397+Yukics@users.noreply.github.com>
2021-10-07 18:07:18 +0200

    19 hecho

---
+++ Practica 2 - Repaso SQL/19.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT c.customer_id, c.company_name, c.contact_name FROM customers AS c 
+INNER JOIN (SELECT * FROM orders WHERE order_date >= '1997-01-01' OR order_date < '1996-01-01') AS o
+ON o.customer_id=c.customer_id
