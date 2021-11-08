d63055c72d159798b32ea02a8ff728c14c3ace05
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-08 19:33:04 +0100

    Update 19.sql

--- Practica 2 - Repaso SQL/19.sql
+++ Practica 2 - Repaso SQL/19.sql
@@ -6,3 +6,8 @@ ON o.customer_id=c.customer_id
 SELECT DISTINCT c.customer_id, c.contact_name, c.company_name FROM customers AS c
 INNER JOIN (SELECT * FROM orders WHERE order_date >= '1996-01-01' AND order_date < '1997-01-01') AS o
 ON c.customer_id!=o.customer_id
+
+-- Intento correcto post corrección
+SELECT  customer_id, contact_name, company_name FROM customers 
+where customer_id not in (SELECT Distinct customer_id as cid FROM orders WHERE order_date >= '1996-01-01' AND
+order_date < '1997-01-01')
