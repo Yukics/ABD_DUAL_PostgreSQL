ccf9fdff17fba73b068a0fa4994d36a352e5c598
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-16 18:55:10 +0100

    2.3 finished

---
+++ Practica 4 - Vistas y reglas/2.3.sql
@@ -0,0 +1,25 @@
+-- Creamos una vista
+
+CREATE VIEW v_lastTenOrders AS
+SELECT order_id, customer_id, ship_name FROM public.orders
+ORDER BY order_date DESC LIMIT 10
+
+-- Creamos la regla UPDATE
+
+CREATE RULE r_updateLastOrders AS ON UPDATE TO public.v_lastTenOrders
+    DO INSTEAD
+    UPDATE public.v_lastTenOrders
+       SET order_id = NEW.order_id,
+           customer_id = NEW.customer_id,
+           ship_name = NEW.ship_name,
+     WHERE order_id = OLD.order_id;
+
+-- Creamos la regla INSERT
+
+CREATE RULE r_insertLastOrders AS ON INSERT TO public.v_lastTenOrders
+    DO INSTEAD
+    INSERT INTO public.v_lastTenOrders VALUES (
+        NEW.order_id,
+        NEW.customer_id,
+        NEW.ship_name
+    );
