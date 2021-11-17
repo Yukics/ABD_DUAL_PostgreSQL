7f69d4f767133522429c0de5ec0e8be12be8184d
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-17 18:04:13 +0100

    Update database

--- Practica 5 - Triggers/2 - Create database.sql
+++ Practica 5 - Triggers/2 - Create database.sql
@@ -6,18 +6,25 @@ CREATE TABLE persones (
     data_naixement date,
     estat estat_civil NOT NULL,
     num_casat int NOT NULL,
-    num_divorciat int NOT NULL
+    num_divorciat int NOT NULL,
+    PRIMARY KEY(id_persona)
 );
 
 CREATE TABLE casats (
     persona1 int NOT NULL,
     persona2 int NOT NULL,
-    data_casament date NOT NULL
+    data_casament date NOT NULL,
+    PRIMARY KEY(persona1, persona2),
+    CONSTRAINT fk_persona1_casats FOREIGN KEY(persona1) REFERENCES persones(id_persona),
+    CONSTRAINT fk_persona2_casats FOREIGN KEY(persona2) REFERENCES persones(id_persona)
 );
 
 CREATE TABLE divorciats (
     persona1 int NOT NULL,
     persona2 int NOT NULL,
-    data_divorci date NOT NULL
+    data_divorci date NOT NULL,
+    PRIMARY KEY(persona1, persona2),
+    CONSTRAINT fk_persona1_divorciats FOREIGN KEY(persona1) REFERENCES persones(id_persona),
+    CONSTRAINT fk_persona2_divorciats FOREIGN KEY(persona2) REFERENCES persones(id_persona)
 );
 
