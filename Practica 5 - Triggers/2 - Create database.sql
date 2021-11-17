bf98537a4441ccd3fc9b6da6c9d7e02013cf1d1b
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-17 18:52:03 +0100

    Update insert

--- Practica 5 - Triggers/2 - Create database.sql
+++ Practica 5 - Triggers/2 - Create database.sql
@@ -32,11 +32,11 @@ CREATE TABLE divorciats (
     CONSTRAINT fk_persona2_divorciats FOREIGN KEY(persona2) REFERENCES persones(id_persona)
 );
 
-INSERT INTO persones VALUES(1, 'Paquita', 'Salas', 1999-01-02, 'solter', 0, 0);
-INSERT INTO persones VALUES(2, 'Obi', 'WAN LAN', 1998-02-03, 'solter', 0, 0);
-INSERT INTO persones VALUES(3, 'Coco', 'Cabra', 1989-10-12, 'solter', 0, 0);
-INSERT INTO persones VALUES(4, 'Jaime', 'Segundo', 1996-02-03, 'solter', 0, 0);
-INSERT INTO persones VALUES(5, 'Juan', 'Juan', 1994-05-05, 'solter', 0, 0);
-INSERT INTO persones VALUES(6, 'Robin', 'Hood', 1995-12-07, 'solter', 0, 0);
-INSERT INTO persones VALUES(7, 'Rosalia', 'Tratra', 1996-11-12, 'solter', 0, 0);
-INSERT INTO persones VALUES(8, 'Francisco', 'Cisco', 1992-12-18, 'solter', 0, 0);
+INSERT INTO persones VALUES(1, 'Paquita', 'Salas', '1999-01-02', 'solter', 0, 0);
+INSERT INTO persones VALUES(2, 'Obi', 'WAN LAN', '1998-02-03', 'solter', 0, 0);
+INSERT INTO persones VALUES(3, 'Coco', 'Cabra', '1989-10-12', 'solter', 0, 0);
+INSERT INTO persones VALUES(4, 'Jaime', 'Segundo', '1996-02-03', 'solter', 0, 0);
+INSERT INTO persones VALUES(5, 'Juan', 'Juan', '1994-05-05', 'solter', 0, 0);
+INSERT INTO persones VALUES(6, 'Robin', 'Hood', '1995-12-07', 'solter', 0, 0);
+INSERT INTO persones VALUES(7, 'Rosalia', 'Tratra', '1996-11-12', 'solter', 0, 0);
+INSERT INTO persones VALUES(8, 'Francisco', 'Cisco', '1992-12-18', 'solter', 0, 0);
