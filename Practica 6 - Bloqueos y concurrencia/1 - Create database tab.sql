f3e9501967392434312b1b4e9a1b7d86b32f695e
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-24 18:02:23 +0100

    New Activity

---
+++ Practica 6 - Bloqueos y concurrencia/1 - Create database tab.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tab (
+    id int NOT NULL,
+    text character varying(200) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO "tab" VALUES(1,"cococabra");
+INSERT INTO "tab" VALUES(2,"fruta hana hana");
