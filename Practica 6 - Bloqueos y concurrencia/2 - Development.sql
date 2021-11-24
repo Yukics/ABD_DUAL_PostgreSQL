311eee9f791c3bbbd73c1d30ebb9091185dfa331
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-11-24 18:08:23 +0100

    Update

--- Practica 6 - Bloqueos y concurrencia/2 - Development.sql
+++ Practica 6 - Bloqueos y concurrencia/2 - Development.sql
@@ -10,4 +10,22 @@ UPDATE "tab" SET "text"='SUUUUUU' WHERE id=2;
 -- 5 
 BEGIN TRANSACTION;
 
-UPDATE "tab" SET "text"='SUUUUUU' WHERE id=2;
+UPDATE "tab" SET "text"='NOOOOOO' WHERE id=2;
+
+-- 6
+SELECT pid, 
+
+       usename, 
+
+       pg_blocking_pids(pid) as blocked_by, 
+
+       query as blocked_query
+
+FROM pg_stat_activity
+
+WHERE cardinality(pg_blocking_pids(pid)) > 0;
+
+-- 7
+BEGIN TRANSACTION;
+
+UPDATE "tab" SET "text"='MAAAAOMENO' WHERE id=2;
