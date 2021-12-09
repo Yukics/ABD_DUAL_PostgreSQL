1db1f8fbca90cdd0072795256467757a50e7e4b5
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2021-12-09 21:27:11 +0100

    Update 3 - Triggers.sql

--- Practica 5 - Triggers/3 - Triggers.sql
+++ Practica 5 - Triggers/3 - Triggers.sql
@@ -35,3 +35,21 @@ CREATE OR REPLACE FUNCTION f_deleteCasats() RETURNS trigger AS $$
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER t_deleteCasats AFTER DELETE ON casats FOR EACH ROW EXECUTE PROCEDURE f_deleteCasats();
+
+
+--Trigger Examen
+CREATE OR REPLACE FUNCTION f_insertDiv() RETURNS trigger AS $$
+    BEGIN
+
+        
+        UPDATE persones SET estat = 'divorciat', num_divorciat = num_divorciat + 1 WHERE OLD.persona1=id_persona or OLD.persona2=id_persona;
+
+		DELETE FROM casats where (OLD.persona1=casats.persona1 and OLD.persona2=casats.persona2) or (OLD.persona2=casats.persona1 and OLD.persona1=casats.persona2);
+
+        RETURN OLD;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER t_insertDiv AFTER INSERT ON divorciats FOR EACH ROW EXECUTE PROCEDURE f_insertDiv();
+
+INSERT INTO divorciats values(3,5,'2016-01-01', '2016-01-02');
